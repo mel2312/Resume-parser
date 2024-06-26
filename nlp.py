fc81c0301bd1eecc50f61bd36a3386ca23df69f8
Melwin P Sebastian <melwinperumpral@gmail.com>
2024-06-26 09:13:26 -0600

    some warning while doing tokenisation

--- nlp.py
+++ nlp.py
@@ -14,8 +14,17 @@ def clean_text(text):
 
 data['cleaned_resume'] = data['Resume'].apply(clean_text)
 
+#nltk.data.find('stopword')
 nltk.download('stopwords')
 nltk.download('punkt')
 
 stop_words = set(stopwords.words('english'))
 
+def tokenize_and_remove_stopwords(text):
+    words = nltk.word_tokenize(text)
+    words = [word for word in words if word not in stop_words]
+    return words
+
+data['tokenized_text'] = data['cleaned_resume'].apply(tokenize_and_remove_stopwords)
+
+print(data['tokenized_text'])
