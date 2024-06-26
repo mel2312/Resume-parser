869719ac5277293b1e36899a67246567cc5563a9
Melwin P Sebastian <melwinperumpral@gmail.com>
2024-06-26 07:27:07 -0600

    imported nltk thinking about stemming

--- nlp.py
+++ nlp.py
@@ -1,5 +1,21 @@
 import pandas as pd
+import re
+import nltk
+from nltk.corpus import stopwords
 
 data = pd.read_csv('UpdatedResumeDataSet.csv')
-#print(data.head())
-print(data["Resume"])
+
+
+def clean_text(text):
+    text = re.sub(r'\W', ' ', text)  # Remove all non-word characters
+    text = re.sub(r'\s+', ' ', text)  # Remove extra spaces
+    return text.lower()
+
+
+data['cleaned_resume'] = data['Resume'].apply(clean_text)
+
+nltk.download('stopwords')
+nltk.download('punkt')
+
+stop_words = set(stopwords.words('english'))
+
