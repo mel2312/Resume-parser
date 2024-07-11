af904e674806df34ac0b2277fcc4c6ce02ee8c6d
Melwin P Sebastian <melwinperumpral@gmail.com>
2024-07-11 12:13:12 -0600

    Added TF-IDF vectorizer working finally

--- nlp.py
+++ nlp.py
@@ -2,6 +2,9 @@ import pandas as pd
 import re
 import nltk
 from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 data = pd.read_csv('UpdatedResumeDataSet.csv')
 
@@ -13,18 +16,24 @@ def clean_text(text):
 
 
 data['cleaned_resume'] = data['Resume'].apply(clean_text)
-
+#subornodebbappon20@gmail.com
 #nltk.data.find('stopword')
-nltk.download('stopwords')
-nltk.download('punkt')
+#nltk.download('stopwords')
+#nltk.download('punkt')
 
 stop_words = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
 
 def tokenize_and_remove_stopwords(text):
-    words = nltk.word_tokenize(text)
-    words = [word for word in words if word not in stop_words]
-    return words
+    words = word_tokenize(text)
+    words = [lemmatizer.lemmatize(word.lower()) for word in words if word.lower() not in stop_words and word.isalpha()]
+    return '.'.join(words)
 
 data['tokenized_text'] = data['cleaned_resume'].apply(tokenize_and_remove_stopwords)
+assert data['tokenized_text'].apply(lambda x: isinstance(x, str)).all(), "Not all processed resumes are strings"
 
-print(data['tokenized_text'])
+print(type(data['tokenized_text']))
+
+tfidf_vectorizer = TfidfVectorizer(max_features=3000)
+tfidf_matrix = tfidf_vectorizer.fit_transform(data['tokenized_text'])
+print(tfidf_vectorizer.vocabulary_)
